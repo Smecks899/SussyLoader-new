935088b5bf9c3c7f6797df5ef818a11c81d75db2
Smecks899 <lonasweatfix@gmail.com>
2025-03-19 18:27:26 +0200

    Update and rename README.md to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,11 @@
+--loader 
+local games = {
+    15437737400
+}
+
+for i,v in games do 
+    if game.PlaceId == v then 
+        print('True')
+        break
+    end 
+end 
--- README.md
+++
@@ -1,1 +0,0 @@
-# SussyLoader-new
