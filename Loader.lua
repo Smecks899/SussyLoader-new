0893e962e45210bc9f89e50ef243039a2c3d413c
Smecks899 <lonasweatfix@gmail.com>
2025-03-26 23:16:47 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,7 @@
 local games = {
-    ["15437737400"] = "https://raw.githubusercontent.com/Smecks899/SussyLoader-new/refs/heads/main/Chaos(Remastered).lua"
+    ["15437737400"] = "https://raw.githubusercontent.com/Smecks899/SussyLoader-new/refs/heads/main/Chaos(Remastered).lua",
+	["73956553001240"] = "https://raw.githubusercontent.com/Smecks899/SussyLoader-new/refs/heads/main/Volleyball%20Legends.lua
+	
 }
 local placeId = tostring(game.PlaceId)
 
